954699766357e21d55d75a1c0113ffe83cd3c81a
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-11-27 18:48:06 +0100

    Downgrade python to 3.8 in docs_deployment

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#ghp-import -n -p -f -c cpp-adv.infotraining.pl _build/html/
-ghp-import -n -p -f _build/html/
+ghp-import -n -p -f -c cpp-adv.infotraining.pl _build/html/
+#ghp-import -n -p -f _build/html/
